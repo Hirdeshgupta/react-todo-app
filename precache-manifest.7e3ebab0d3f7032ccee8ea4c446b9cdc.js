2e0c13305d867d7b6f6b7266cd4a68143b563328
Hirdeshgupta <hirdeshgupta16@gmail.com>
2020-06-28 05:56:52 +0530

    Updates

---
+++ precache-manifest.7e3ebab0d3f7032ccee8ea4c446b9cdc.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "843444934af39dba28b0227c6862c8c0",
+    "url": "/React-To-Do-App/index.html"
+  },
+  {
+    "revision": "195c64d57c21cb3ab829",
+    "url": "/React-To-Do-App/static/css/2.2cbb2c5d.chunk.css"
+  },
+  {
+    "revision": "a8e85408915afa0c933c",
+    "url": "/React-To-Do-App/static/css/main.c53315a7.chunk.css"
+  },
+  {
+    "revision": "195c64d57c21cb3ab829",
+    "url": "/React-To-Do-App/static/js/2.7dfce3d8.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/React-To-Do-App/static/js/2.7dfce3d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8e85408915afa0c933c",
+    "url": "/React-To-Do-App/static/js/main.b9d8416a.chunk.js"
+  },
+  {
+    "revision": "83e5fbac70e5b162459f",
+    "url": "/React-To-Do-App/static/js/runtime-main.41b35dd6.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/React-To-Do-App/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/React-To-Do-App/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "4a5d4d855d11fae79cc864bdacb2f479",
+    "url": "/React-To-Do-App/static/media/fa-brands-400.4a5d4d85.eot"
+  },
+  {
+    "revision": "4c1da237bdae0773309df93b2cd48e09",
+    "url": "/React-To-Do-App/static/media/fa-brands-400.4c1da237.ttf"
+  },
+  {
+    "revision": "5734d789b25228cbafc64a58ae971aca",
+    "url": "/React-To-Do-App/static/media/fa-brands-400.5734d789.woff"
+  },
+  {
+    "revision": "778b1f251bea7412048da95b87bf816f",
+    "url": "/React-To-Do-App/static/media/fa-brands-400.778b1f25.svg"
+  },
+  {
+    "revision": "91a23e8bf2b4b84c39311cb5eb23aaa0",
+    "url": "/React-To-Do-App/static/media/fa-brands-400.91a23e8b.woff2"
+  },
+  {
+    "revision": "260be4f29c0b2ce47480afb23f38f237",
+    "url": "/React-To-Do-App/static/media/fa-regular-400.260be4f2.ttf"
+  },
+  {
+    "revision": "5c674c9216c06ede2f618aa58ae71116",
+    "url": "/React-To-Do-App/static/media/fa-regular-400.5c674c92.woff2"
+  },
+  {
+    "revision": "66578cdbb6dc01f527a53971051b3e85",
+    "url": "/React-To-Do-App/static/media/fa-regular-400.66578cdb.svg"
+  },
+  {
+    "revision": "6b20949b3a679c30d09f64acd5d3317d",
+    "url": "/React-To-Do-App/static/media/fa-regular-400.6b20949b.eot"
+  },
+  {
+    "revision": "d44ad00c44e46fd29f6126fa7d888cde",
+    "url": "/React-To-Do-App/static/media/fa-regular-400.d44ad00c.woff"
+  },
+  {
+    "revision": "412a43d6840addd683665ec12c30f810",
+    "url": "/React-To-Do-App/static/media/fa-solid-900.412a43d6.woff2"
+  },
+  {
+    "revision": "486853107489520b3265b19b191626f8",
+    "url": "/React-To-Do-App/static/media/fa-solid-900.48685310.svg"
+  },
+  {
+    "revision": "9a1672a8a8d91fbf82c71f451d495253",
+    "url": "/React-To-Do-App/static/media/fa-solid-900.9a1672a8.eot"
+  },
+  {
+    "revision": "c65d154888aa166982dac3e72e7380ec",
+    "url": "/React-To-Do-App/static/media/fa-solid-900.c65d1548.ttf"
+  },
+  {
+    "revision": "f3a7d3b5880544a91e9a7e6f8f35d4d2",
+    "url": "/React-To-Do-App/static/media/fa-solid-900.f3a7d3b5.woff"
+  }
+]);
